406dbe87c848a927f407e37c04e49cc567e0cbdc
Michael Milroy <mdmilroy@gmail.com>
2020-04-10 20:06:17 -0400

    first commit badges

---
+++ KomodoCorp/KomodoBadges/KomodoBadges/Badges.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBadges
+{
+    public class Badges
+    {
+        public int badgeID;
+        public List<string> badgeAccess;
+
+        public Badges(int id, List<string> doors)
+        {
+            badgeID = id;
+            badgeAccess = doors;
+        }
+    }
+}
---
+++ KomodoCorp/KomodoBadges/KomodoBadges/BadgesRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBadges
+{
+    public class BadgesRepo
+    {
+        private List<Badges> _badges = new List<Badges>();
+
+
+    }
+}
