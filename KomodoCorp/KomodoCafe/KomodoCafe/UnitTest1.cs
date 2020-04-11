9b6e8a6432accd2e57b2c9933d41ada9662a5fb9
Michael Milroy <mdmilroy@gmail.com>
2020-04-10 23:36:02 -0400

    updated test file

--- KomodoCorp/KomodoCafe/KomodoCafe/UnitTest1.cs
+++ KomodoCorp/KomodoCafe/KomodoCafe/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KomodoCafe
@@ -9,7 +11,17 @@ namespace KomodoCafe
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            MenuRepo menu = new MenuRepo();
+            List<Menu> currentItems = new List<Menu>();
 
+            // Act
+            Menu menu1 = new Menu(1, "cheeseburger", "burger with fries and drink", "bun, beef, pickles, cheese", 4.99);
+            currentItems.Add(menu1);
+
+            // Assert
+            int actual = currentItems.Count();
+            Assert.AreEqual(1, actual);
         }
     }
 }
