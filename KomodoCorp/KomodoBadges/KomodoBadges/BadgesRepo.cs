35515d805d489153c0f58e643bc607441a2b05c9
Michael Milroy <mdmilroy@gmail.com>
2020-04-10 20:51:52 -0400

    added CreateBadge functionality

--- KomodoCorp/KomodoBadges/KomodoBadges/BadgesRepo.cs
+++ KomodoCorp/KomodoBadges/KomodoBadges/BadgesRepo.cs
@@ -9,7 +9,50 @@ namespace KomodoBadges
     public class BadgesRepo
     {
         private List<Badges> _badges = new List<Badges>();
+        private Dictionary<int, List<string>> allBadges = new Dictionary<int, List<string>>();
 
+        public void CreateBadge()
+        {
+            Console.Clear();
+            Random randID = new Random();
+            int idnum = randID.Next(1000, 10000);
+            List<string> access = new List<string>();
+            bool moreDoors = true;
 
+            while(moreDoors)
+            {
+                Console.WriteLine("\nWhat door accessible?");
+                string doors = Console.ReadLine();
+                access.Add(doors);
+                Console.WriteLine("\nMore doors?");
+                string r = Console.ReadLine().ToLower();
+
+                if (r == "n" || r == "no")
+                {
+                    moreDoors = false;
+                }
+                else if (r != "y" && r != "yes")
+                {
+                    Console.WriteLine("\nPlease enter [y] or [n]");
+                }
+            }
+            Badges newBadge = new Badges(idnum, access);
+            allBadges.Add(newBadge.badgeID, newBadge.badgeAccess);
+            
+            if (allBadges.ContainsKey(newBadge.badgeID))
+            {
+                Console.Clear();
+                Console.WriteLine($"You have successfully created badge number {newBadge.badgeID.ToString()}" +
+                    $" with access to doors {newBadge.badgeAccess.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine("Your badge creation was unsuccessful. Please try again.");
+            }
+
+            Console.WriteLine("Press any key to return to the main menu.");
+            Console.ReadLine();
+            Console.Clear();
+        }
     }
 }
---
+++ KomodoCorp/KomodoBadges/KomodoBadges/ProgramUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBadges
+{
+    public class ProgramUI
+    {
+        public void Run()
+        {
+            BadgesRepo _badges = new BadgesRepo();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Welcome. Please choose option:\n" +
+                    "1. Create a new badge.\n");
+                string r = Console.ReadLine();
+                if (r == "1")
+                {
+                    _badges.CreateBadge();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Please choose a valid option.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+
+        }
+    }
+}
